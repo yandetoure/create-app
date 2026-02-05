d85974c2e2b5dcdc19f29ab9819059f2e30e126e
yandetoure <ndeyeyandemouhammad@gmail.com>
2026-02-05 01:19:39 +0000

    newapp

--- database/migrations/2026_02_05_000418_create_feature_project_table.php
+++ database/migrations/2026_02_05_000418_create_feature_project_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('project_feature', function (Blueprint $table) {
+        Schema::create('feature_project', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project_id')->constrained()->cascadeOnDelete();
             $table->foreignId('feature_id')->constrained()->cascadeOnDelete();
@@ -23,6 +23,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('project_feature');
+        Schema::dropIfExists('feature_project');
     }
 };
